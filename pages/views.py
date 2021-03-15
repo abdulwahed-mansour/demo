cd1534f5d2f82e0a746f960012dcce03b611e1eb
abdulwahed-mansour <73190425+abdulwahed-mansour@users.noreply.github.com>
2021-03-15 23:51:32 +0200

    if Age == 30

--- pages/views.py
+++ pages/views.py
@@ -4,7 +4,6 @@ from .models import Product
 from datetime import datetime
 from django.views import generic
 
-from datetime import datetime
 import pytz
 import random
 
@@ -15,24 +14,15 @@ class HomePageView(TemplateView):
     
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
-        # random.randrange(start, stop, step) 
-        context['number'] = random.randrange(0, 100, 5)
-        context['local'] = datetime.now().strftime("%m/%d/%Y, %H:%M")
         
-        tz_NY = pytz.timezone('America/New_York') 
-        datetime_NY = datetime.now(tz_NY)
-        context['newyourk'] = datetime_NY.strftime("%m/%d/%Y, %H:%M:%S")
-        context['calculation'] = str(8 + 7)
-        basket = ['apple', 'orange', 'apple', 'pear', 'orange', 'banana']
-       
-        context['mybaskt'] = basket 
         
         x = '{ "name":"John", "age":30, "city":"New York"}'
         y = json.loads(x)
         
         context['Name'] = y["name"].upper()
-        context['Age'] = y["age"] * 5
+        context['Age'] = y["age"]
         context['City'] = y["city"]
+
       
         # the result is a Python dictionary: print(y["age"])
         return context
